1e2d8ef3d5dfcff81c7bd00bd74c2c6d7e4f4c01
Charles Hill <chill@degreesofzero.com>
2018-07-23 18:26:15 +0200

    Pass app to controllers and middleware

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = module.exports = express();
 
 app.config = require('./config');
-require('./middleware');
-require('./controllers');
+require('./middleware')(app);
+require('./controllers')(app);
 
 app.server = app.listen(app.config.port, app.config.host, function() {
 	console.log('Server listening at ' + app.config.host + ':' + app.config.port);
