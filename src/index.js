d81c8e162c7dd9f9aa14cde3b887508c3dc0ebfb
Robin Cussol <robin@loki.local>
2018-07-23 19:07:49 +0200

    Adding routing to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
+import Home from './routes/home'
+import TournamentResults from './routes/tournament-results'
+import NotFound from './routes/not-found'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/tournament-results" component={TournamentResults} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 registerServiceWorker()
---
+++ src/routes/tournament-results.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const TournamentResults = () => <div>This is the tournament results page</div>
+
+export default TournamentResults
